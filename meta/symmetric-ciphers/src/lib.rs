adbce0ce3a1fac6ff9892318229151046bd94092
Артём Павлов [Artyom Pavlov] <newpavlov@gmail.com>
2016-09-28 22:35:35 +0300

    Updated meta

--- meta/symmetric-ciphers/src/lib.rs
+++ meta/symmetric-ciphers/src/lib.rs
@@ -1,9 +1,11 @@
 #![no_std]
 pub extern crate blowfish;
 pub extern crate chacha20;
+pub extern crate chacha20poly1305;
 pub extern crate hc128;
-pub extern crate sosemanuk;
-pub extern crate salsa20;
 pub extern crate rc4;
+pub extern crate salsa20;
+pub extern crate sosemanuk;
 
 pub extern crate crypto_symmetric;
+pub extern crate crypto_aead;
